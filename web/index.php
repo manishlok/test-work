24f3fc60fecd72ef3a532b5c9ba4fe34b2ee4e38
Manish Sahu <manishnfriend@gmail.com>
2020-09-07 14:22:58 +0545

    redirect wp

--- web/index.php
+++ web/index.php
@@ -2,5 +2,11 @@
 /**
  * WordPress View Bootstrapper
  */
-define('WP_USE_THEMES', true);
-require __DIR__ . '/nero/wp-blog-header.php';
+if (preg_match('/\/?wp-(.*)$/', $_SERVER["REQUEST_URI"], $matches)) {
+    header('Location: https://cyberpur.com');
+} elseif (($_SERVER['REQUEST_URI'] == '/club') || (preg_match('/\/?club\/(.*)$/', $_SERVER["REQUEST_URI"], $matches))) {
+    header('Location: https://cyberpur.com');
+} else {
+    define('WP_USE_THEMES', true);
+    require __DIR__ . '/nero/wp-blog-header.php';
+}
